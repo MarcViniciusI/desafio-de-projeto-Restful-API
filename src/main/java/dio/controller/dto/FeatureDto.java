ebeae1335e28fc0d109196a27e6ba332ad2c2df3
MarcViniciusI <marcosviniciusinacio@outlook.com>
2024-11-17 14:39:21 -0300

    melhorias , controle de exceções

---
+++ src/main/java/dio/controller/dto/FeatureDto.java
@@ -0,0 +1,18 @@
+package dio.controller.dto;
+
+import dio.model.Feature;
+
+public record FeatureDto(Long id, String icon, String description) {
+
+    public FeatureDto(Feature model) {
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public Feature toModel() {
+        Feature model = new Feature();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
+}
---
+++ src/main/java/dio/service/UserService.java
@@ -0,0 +1,6 @@
+package dio.service;
+import dio.model.User;
+
+public interface UserService extends CrudService<Long, User> {
+
+}
